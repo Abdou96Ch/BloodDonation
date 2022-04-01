2e7e4330625bf1263673fc49179bab2f8d2724a5
Your Name <ouassim.amri.11@gmail.com>
2022-04-01 10:27:24 +0000

    loginactivity : j ai ajouter la fonction fonctonTest

--- app/src/main/java/com/example/blooddonationapp/LoginActivity.java
+++ app/src/main/java/com/example/blooddonationapp/LoginActivity.java
@@ -10,6 +10,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //teste commentqire wassim
     }
+
+    /*public fonctionTest(){
+        return 0;
+ }*/
 }
